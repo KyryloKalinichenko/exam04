323bfffeabd3aecefa7b27606e7f00f5affa5dc4
Kyrylo Kalinichenko <kirillstomper01@gmail.com>
2022-04-08 13:01:56 +0200

    better

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ typedef struct lst_s {
 
 	lst_t*	pr;
 	lst_t*	next;
+	int	fd[2];
 	char**	cmd;
 	char	type;
 
@@ -40,6 +41,7 @@ void	print_err(char* str){
 
 void count(main_t* stru)
 {
+	stru->cnt = 0;
 	for (int i = 1; i < stru->argc; i++){
 		if (!strcmp(stru->argv[i], "|")
 			|| !strcmp(stru->argv[i], ";"))
@@ -72,16 +74,19 @@ void lst_create(main_t* stru){
 		print_err("error: fatal\n");
 		exit(EXIT_FAILURE);
 	}
-
+	stru->lst = tmp;
 	tmp->next = NULL;
 	tmp->pr = NULL;
 	tmp->cmd = &stru->argv[1];
 
 	if (stru->cnt == 0)
 		tmp->type = ';';
-	else
-		tmp->type = stru->argv[stru->pos[0]];
+	else{
+		tmp->type = *stru->argv[stru->pos[0]];
+		stru->argv[stru->pos[0]] = NULL;
+	}
 	pr = tmp;
+
 	for (int i = 1; i <= stru->cnt; i++)
 	{
 		tmp = malloc(sizeof(lst_t));
@@ -92,13 +97,95 @@ void lst_create(main_t* stru){
 		}
 		tmp->next = NULL;
 		tmp->pr = pr;
+
 		pr->next = tmp;
 
-		tmp->cmd = stru->argv[stru->pos[i-1]+1];
-
-		
+		tmp->cmd = &stru->argv[stru->pos[i-1]+1];
+		if (i == stru->cnt)
+			tmp->type = ';';
+		else{
+			tmp->type = *stru->argv[stru->pos[i]];
+			stru->argv[stru->pos[i]] = NULL;
+		pr = tmp;
+		}
 	}
 
+}
+
+void exec(char** cmd, char** env){
+	if (-1 == execve(*cmd, cmd, env))
+	{
+		print_err("error: cannot execute ");
+		print_err(*cmd);
+		print_err("\n");
+
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	comm(main_t* stru, lst_t* curr){
+	if(curr->type == '|' && -1 == pipe(curr->fd))
+	{
+		print_err("error: fatal\n");
+		exit(EXIT_FAILURE);
+	}
+	pid_t pid = fork();
+	int stat;
+	if (!pid){
+		if (curr->type == '|' && dup2(curr->fd[1], 1) == -1)
+		{
+			print_err("error: fatal\n");
+			exit(EXIT_FAILURE);
+		}
+		if ((curr->pr && curr->pr->type == '|') && dup2(curr->pr->fd[0], 0) == -1)
+		{
+			print_err("error: fatal\n");
+			exit(EXIT_FAILURE);
+		}
+		exec(curr->cmd, stru->env);
+	}
+	else if(pid > 0){
+		waitpid(pid, &stat, 0);
+		if (curr->type == '|')
+			close(curr->fd[1]);
+		if (curr->pr && curr->pr->type == '|')
+			close(curr->pr->fd[0]);
+	}
+	else
+	{
+		print_err("error: fatal\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	lst_use(main_t* stru){
+	lst_t* curr = stru->lst;
+	while(curr){
+		//printf("TYPE %c CMD %s\n", curr->type, *curr->cmd);
+		if (*curr->cmd == NULL)
+		{
+			curr = curr->next;
+			continue;
+		}
+		else if (!strcmp(*curr->cmd, "cd"))
+		{
+			if (!curr->cmd[1] || curr->cmd[2])
+			{
+				print_err("error: cd: bad arguments\n");
+				exit(EXIT_FAILURE);
+			}
+			if (-1 == chdir(curr->cmd[1]))
+			{
+				print_err("error: cd: cannot change directory to \n");
+				print_err(curr->cmd[1]);
+				print_err("\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+			comm(stru, curr);
+		curr = curr->next;
+	}
 }
 
 int main(int argc, char** argv, char** env)
@@ -114,6 +201,8 @@ int main(int argc, char** argv, char** env)
 	
 	count(&stru);
 	pos(&stru);
+	lst_create(&stru);
+	lst_use(&stru);
 
 	exit(0);
 
